586c94faa1d71bf73812b0f1a9cc6c864b5931ac
deucenn <panix.black@web.de>
2025-01-20 10:29:40 +0100

    add: flask

--- src/index.py
+++ src/index.py
@@ -1,7 +1,27 @@
-from flask import Flask
+from flask import Flask, jsonify, request
+from sqlalchemy import create_engine
+from geoalchemy2 import Geometry
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+engine = create_engine('postgresql://user:password@host:port/database')
+
+@app.route('/route', methods=['GET'])
+def get_route():
+    start_point = request.args.get('start')  # z.B. 'SRID=4326;POINT(10.0 50.0)'
+    end_point = request.args.get('end')
+
+    # SQL-Abfrage erstellen (dynamisch mit start_point und end_point)
+    # ... (Hier die SQL-Abfrage einfügen, start_node_id und end_node_id ermitteln)
+
+    with engine.connect() as conn:
+        result = conn.execute(sql_query)
+        rows = result.fetchall()
+
+    # Ergebnis in JSON umwandeln
+    # ... (Hier die Ergebnisse in ein JSON-Format umwandeln)
+
+    return jsonify(result)
+
+if __name__ == '__main__':
+    app.run(debug=True)
